823c024bd1a0a6285c9849ee2e4354fe076f8872
Felipe <--global>
2022-03-10 20:40:57 -0300

    renomeando arquivos

--- frontend/src/components/Navbar2/index.tsx
+++ frontend/src/components/Navbar2/index.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as GithubIcon } from '../../assets/images/github.svg';
+import { ReactComponent as GithubIcon } from 'assets/images/github.svg';
 import './styles.css';
 
 function Navbar() {
